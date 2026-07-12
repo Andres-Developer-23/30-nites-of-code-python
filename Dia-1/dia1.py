ebd1b243debbcc8077df45df382c284d569946de
Andres-Developer-23 <andres01032001@gmail.com>
2026-07-12 18:39:27 -0500

    Hoy aprendí a declarar variables, tipos de datos (int, float, str, bool) y la función print()

---
+++ Dia-1/dia1.py
@@ -0,0 +1,19 @@
+nombre = "Andres Bravo"
+edad = 25
+altura = 1.81
+ciudad = "Cienaga Magdalena"
+
+# mostrar en terminal
+print(nombre)
+print(edad)
+print(altura)
+print(ciudad)
+
+print(type(nombre)) # str
+print(type(edad))   # int
+print(type(altura)) # float
+print(type(ciudad)) # str
+
+# hobby
+hobby = "Todo lo relacionado con Pokemon"
+print(hobby)    # str
