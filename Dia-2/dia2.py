e809c418c44e97ca90a58ac8cef5b69f94d981c7
Andres-Developer-23 <andres01032001@gmail.com>
2026-07-12 19:11:26 -0500

    Hoy aprendí Operadores y entrada de datos en Python operadores aritméticos, comparación y lógicos. Practiqué con input() para interactuar con el usuario.

---
+++ Dia-2/dia2.py
@@ -0,0 +1,11 @@
+cantidad_euro = float(input("Ingresa una cantidad en euros: "))
+
+# convertir a usd
+cambio_euro_a_usd = cantidad_euro * 1.14
+
+print(cambio_euro_a_usd)
+
+# verificar si es mayor de edad
+edad = int(input("Ingresa tu edad: "))
+es_mayor = edad >= 18
+print(es_mayor)
